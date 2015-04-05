3fc5a1231d53d4a696105ac6064c16480fca60b2
Ali <alim20@aexxalim20m1.corp.emc.com>
2015-04-05 12:13:21 +0400

    Incorporated dynamic hostnaming

--- prepare_host.sh
+++ prepare_host.sh
@@ -4,6 +4,7 @@ NUMBER_OF_CLUSTER_NODES=$1
 HOSTNAME=$2
 IP_ADDRESS_RANGE=$3
 START_IP=$4
+PHD_HOSTNAME_PREFIX=$5
 
 yum -y install openssl
 
@@ -34,21 +35,21 @@ for i in $(eval echo {1..$NUMBER_OF_CLUSTER_NODES}); do
 
    HOST_IP=$IP_ADDRESS_RANGE$(($START_IP+$i))
    
-   echo $HOST_IP phd$i.localdomain phd$i >> /etc/hosts 
+   echo $HOST_IP $PHD_HOSTNAME_PREFIX$i.localdomain PHD_HOSTNAME_PREFIX$i >> /etc/hosts 
    
    #this mess is to avoid the host authenticity prompt when passwordless ssh connects to a host. making ambari happy.
-   ssh-keygen -R phd$i.localdomain
+   ssh-keygen -R $PHD_HOSTNAME_PREFIX$i.localdomain
    ssh-keygen -R $HOST_IP
-   ssh-keygen -R phd$i.localdomain,$HOST_IP
-   ssh-keyscan -H phd$i.localdomain,$HOST_IP >> ~/.ssh/known_hosts
+   ssh-keygen -R $PHD_HOSTNAME_PREFIX$i.localdomain,$HOST_IP
+   ssh-keyscan -H $PHD_HOSTNAME_PREFIX$i.localdomain,$HOST_IP >> ~/.ssh/known_hosts
    ssh-keyscan -H $HOST_IP >> ~/.ssh/known_hosts
-   ssh-keyscan -H phd$i.localdomain >> ~/.ssh/known_hosts
+   ssh-keyscan -H $PHD_HOSTNAME_PREFIX$i.localdomain >> ~/.ssh/known_hosts
 
 done
 
 
 #NOW Copy the keys for password less SSH
-if [[ $HOSTNAME == *"phd"* ]]
+if [[ $HOSTNAME == *$PHD_HOSTNAME_PREFIX* ]]
 then
     echo "copying the rsa keys"
     cat /vagrant/id_rsa.pub | cat >> ~/.ssh/authorized_keys
